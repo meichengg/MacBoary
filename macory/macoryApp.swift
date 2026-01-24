f5c24c734b8dc842bc17f01ed1f449956a54ee8c
Marco Baeuml <marco@baeuml.io>
2026-01-24 20:05:06 +0100

    Initial Commit

---
+++ macory/macoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  macoryApp.swift
+//  macory
+//
+//  Created by Marco Baeuml on 24/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct macoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
